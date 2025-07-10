f75a1820a0efce8300c79f09e4eb9136e1d949fa
eweix <elliott@weix.us>
2025-07-09 22:41:43 -0500

    remove dotfiles
    dotfiles will be managed using chezmoi instead. Easy to configure, and
    less of a pain.

--- home/dotfiles/.config/nvim/lua/configs/py_lsp.lua
+++
@@ -1,7 +0,0 @@
-local options = {
-  -- This is optional, but allows to create virtual envs from nvim
-  host_python = "/bin/python",
-  default_venv_name = ".venv", -- For local venv
-}
-
-require("py_lsp.nvim").setup(options)
--- home/dotfiles/.config/nvim/lua/options.lua
+++
@@ -1,7 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-
-local o = vim.o
-o.cursorlineopt ='both'     -- enable cursorline!
--- o.relativenumber = true     -- show line numbers relative to cursor
